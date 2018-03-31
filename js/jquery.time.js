ffc2708629aab06f1c6b3bf92733e7e910c9d348
michele de falco <defalco_michele@libero.it>
2018-03-31 14:26:12 +0200

    Update jquery.time.js

--- js/jquery.time.js
+++ js/jquery.time.js
@@ -3,54 +3,68 @@
  
  
  $.fn.time = function(options) {
-            
-            var defaults = {};
-            var options = $.extend(defaults, options);
-            
-            
-           
-            return this.each(function() {
-            var obj = $(this);    
-            obj.html('<div id="secondi"></div><div id="minuti"></div><div id="ore"></div>');
-            var $secondi = $('#secondi');
-            var $ora = $('#ore');
-            var $minuti = $('#minuti');
-            
-            
-            /* function ora esatta per ora esatta*/
-            function oraesatta (a,b,c){
+    
             var dataintera = new Date();
             var HH = dataintera.getHours();
             var MM = dataintera.getMinutes();
-            var SS = dataintera.getSeconds();
-            var degsec = SS*6;
-            var degora = (HH*30)+((1/2)*MM);
-            var degmin = MM*6;
-             
-            a.css({'transform':'rotate('+degsec+'deg)',
-                   '-o-transform':'rotate('+degsec+'deg)',
-                   '-ms-transform':'rotate('+degsec+'deg)',
-                   '-moz-transform':'rotate('+degsec+'deg)',
-                   '-webkit-transform': 'rotate('+degsec+'deg)'}); 
+            var SS = dataintera.getSeconds(); 
             
-            b.css({'transform':'rotate('+degora+'deg)',
-                       '-o-transform':'rotate('+degora+'deg)',
-                       '-ms-transform':'rotate('+degora+'deg)',
-                       '-moz-transform':'rotate('+degora+'deg)',
-                       '-webkit-transform': 'rotate('+degora+'deg)'});
-            
-           c.css({'transform':'rotate('+degmin+'deg)',
-                      '-o-transform':'rotate('+degmin+'deg)',
-                      '-ms-transform':'rotate('+degmin+'deg)',
-                      '-moz-transform':'rotate('+degmin+'deg)',
-                      '-webkit-transform': 'rotate('+degmin+'deg)'});
-             
-                     };
-             /* function oraesatta per ora esatta*/
+            var defaults = {font_size:22 , color:'red'};
+            var options = $.extend(defaults, options);
            
-      oraesatta($secondi , $ora , $minuti);
-     
-      setInterval(function(){oraesatta($secondi,$ora,$minuti)},1000);     
+           
+          
+           
+           
+ 
+           return this.each(function() { 
+          
+           var str = '<span class="hh"></span>&nbsp;:&nbsp;<span class="mm"></span>&nbsp;:&nbsp;<span class="ss"></span>';
+           $(this).html(str).css({'font-size': options.font_size+'px', 'color': options.color}); 
+           
+           if(SS<=9){
+           $(' span.ss').html('0'+SS); 
+           }
+           else{
+            $(' span.ss').html(SS);  
+           };
+           
+           if(MM<=9){
+            $(' span.mm').html('0'+MM);
+            
+           }else{
+            $(' span.mm').html(MM);
+            };
+           
+           if(HH<=9){
+            $(' span.hh').html('0'+HH);
+            
+           }else{
+            $(' span.hh').html(HH);
+            };
+            
+           
+           setInterval(function(){
+           var dataintera = new Date();
+           var HH = dataintera.getHours();
+           var MM = dataintera.getMinutes();
+           var SS = dataintera.getSeconds();
+           
+           if(SS<=9){$('span.ss').html('0'+SS);         
+            } else{$('span.ss').html(SS);  
+           };
+           
+          if(MM<=9){$('span.mm').html('0'+MM);
+            }else{$('span.mm').html(MM);
+            };
+           
+           if(HH<=9){$('span.hh').html('0'+HH);
+            }else{$('span.hh').html(HH);
+            };
+           
+           },1000);
+           
+      
                
     });   
          
